8ba2d71b8827f49f661b090003d4e1dde29d7d11
Radu Suciu <radusuciu@gmail.com>
2014-09-24 14:15:34 -0700

    Update init script to handle seed data insertion

--- db/init.py
+++ db/init.py
@@ -1,22 +1,31 @@
+import sys, os
+sys.path.append('..')
+
 from db import Database
-import os
+
+db = Database()
 
 def main():
+    create_schema()
+    seed_data()
+    db.close()
 
-    db = Database()
+def create_schema():
+    files = [files for root, dirs, files in os.walk('schema')][0]
+    # moving experiments.sql to the end since it depends on other tables
+    files.append(files.pop(files.index('experiments.sql')))
+    import_files('schema', files)
 
-    for root, dirs, files in os.walk('schema'):
-        # moving experiments.sql to the end since it depends on other tables
-        files.append(files.pop(files.index('experiments.sql')))
-        
-        for file in files:
-            if file.endswith('.sql'):
-                with open(os.path.join('schema', file), 'r') as table:
-                    create_sql = table.read()
-                    db.cursor.execute(create_sql)
-                    db.connection.commit()
+def seed_data():
+    import_files('data', [files for root, dirs, files in os.walk('data')][0])
 
-    db.close
+def import_files(path, files):
+    for file in files:
+        if file.endswith('.sql'):
+            with open(os.path.join(path, file), 'r') as table:
+                create_sql = table.read()
+                db.cursor.execute(create_sql)
+                db.connection.commit()
 
 if __name__ == "__main__":
     main()
